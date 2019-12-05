be7c0f7020503fb6cc0c54c5d9417259c1d8a07f
AlexTheMagnus <i72lulem@uco.es>
2019-12-06 00:28:49 +0100

    Changed side selector for checkboxes

--- src/components/Score/EditCard.js
+++ src/components/Score/EditCard.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EditCard extends Component {
     constructor(props) {
         super(props);
+
+        this.refToPointsInput = React.createRef();
         this.state = {
+            value: [],
             name: this.props.estado[0].player,
-            side: 'ace',
-            points: '0'
+            side: "ace",
+            points: "0",
+            sides: [
+                { key: 6, face: "ace" },
+                { key: 5, face: "king" },
+                { key: 4, face: "queen" },
+                { key: 3, face: "jack" },
+                { key: 2, face: "red" },
+                { key: 1, face: "black" }
+            ]
         };
+
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onChange(e, i) {
+        let value = this.state.value.slice();
+        value[i] = e.target.checked;
+
+        for (let j = 0; j < 6; j++) {
+            value[j] = false;
+        }
+
+        value[i] = true;
+        this.setState({
+            value,
+            side: this.state.sides[i].face
+        });
+
+        console.log(this.state);
     }
 
     handleSubmit(e) {
@@ -17,59 +46,86 @@ class EditCard extends Component {
         this.props.onEditCard(this.state);
         this.setState({
             name: this.props.estado[0].player,
-            side: 'ace',
-            points: '0'
+            points: "0"
         });
+        this.refToPointsInput.current.value = "";
 
-    //console.log(this.props.score[this.state.name].ace);
+        //console.log(this.props.score[this.state.name].ace);
     }
 
     handleInputChange(e) {
-        const {value, name} = e.target;
+        const { value, name } = e.target;
         console.log(value, name);
         this.setState({
             [name]: value
         });
+        console.log(this.state.side);
     }
 
     render() {
         return (
             <div className="float-left">
-                <form onSubmit={this.handleSubmit} className="card mt-4" style={{ background: '#3a3d41' }}>
-                    <div className="card-title text-center" style={{ background: '#1A1D20' }}>
-                        <h3 style={{ color: 'gold' }}><strong>Add Score</strong></h3>
-                    </div>
-                    <div className="form-group">
-                            <select
-                                name="name"
-                                className="form-control"
-                                value={this.state.name}
-                                onChange={this.handleInputChange}
-                            >
-                                {this.props.estado.map((x,y) => <option key={y}>{x.player}</option>)}
-                            </select>
+                <form
+                    onSubmit={this.handleSubmit}
+                    className="card mt-4"
+                    style={{ background: "#3a3d41" }}
+                >
+                    <div
+                        className="card-title text-center"
+                        style={{ background: "#1A1D20" }}
+                    >
+                        <h3 style={{ color: "gold" }}>
+                            <strong>Add Score</strong>
+                        </h3>
                     </div>
                     <div className="form-group">
                         <select
-                            name="side"
+                            name="name"
                             className="form-control"
-                            value={this.state.side}
+                            value={this.state.name}
                             onChange={this.handleInputChange}
                         >
-                            <option>ace</option>
-                            <option>king</option>
-                            <option>queen</option>
-                            <option>jack</option>
-                            <option>red</option>
-                            <option>black</option>
+                            {this.props.estado.map((x, y) => (
+                                <option key={y}>{x.player}</option>
+                            ))}
                         </select>
                     </div>
+                    <div className="form-group">
+                        {this.state.sides.map((item, i) => {
+                            return (
+                                <div>
+                                    <ul
+                                        style={{
+                                            display: "block",
+                                            float: "left",
+                                            width: "28%"
+                                        }}
+                                    >
+                                        <li style={{ display: "block" }}>
+                                            <input
+                                                checked={this.state.value[i]}
+                                                type="checkbox"
+                                                onChange={e =>
+                                                    this.onChange(e, i)
+                                                }
+                                            />
+                                            <div>
+                                                <label>{item.face}</label>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            );
+                        })}
+                    </div>
+
                     <div className="form-group">
                         <input
                             type="text"
                             name="points"
                             className="form-control"
-                            value={this.state.points}
+                            ref={this.refToPointsInput}
+                            //value={this.state.points}
                             onChange={this.handleInputChange}
                             placeholder="Points"
                         />
@@ -79,9 +135,8 @@ class EditCard extends Component {
                     </button>
                 </form>
             </div>
-        )
+        );
     }
-
 }
 
 export default EditCard;
--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { scores } from '../../score.json'
+import { scores } from "../../score.json";
 //console.log(score);
 
-import AddCard from './AddCard';
-import EditCard from './EditCard';
+import AddCard from "./AddCard";
+import EditCard from "./EditCard";
 
 class Score extends Component {
     constructor() {
         super();
         this.state = {
             scores //Es lo mimso que "score: score"
-        }
+        };
         this.handleAddCard = this.handleAddCard.bind(this);
         this.handleEditCard = this.handleEditCard.bind(this);
     }
 
     //Delete player
-    removeCard(index){
+    removeCard(index) {
         this.setState({
             scores: this.state.scores.filter((j, i) => {
-                return i !== index
+                return i !== index;
             })
-        })
+        });
     }
 
     handleEditCard(data) {
-        //console.log(data);
-        var cont = 0;       //Variable solo usada para debug
         var index = 0;
         var auxdict;
 
-        this.state.scores.forEach((score) => {
+        this.state.scores.forEach(score => {
             if (score.player === data.name) {
-                //console.log(cont); //Calcula el indice
                 //var auxdict = score;
                 auxdict = JSON.stringify(score);
                 auxdict = JSON.parse(auxdict);
-                //console.log("este es ", auxdict); //Checking copy
-                cont = index;
-                //index = cont;
 
                 //Deleting old card
                 this.setState({
                     scores: this.state.scores.splice(index, 1)
-                })
+                });
             }
-            //cont++;
             index++;
         });
 
@@ -61,47 +54,78 @@ class Score extends Component {
         //console.log(this.state.scores);
     }
 
-
-
     //Add player
     handleAddCard(score) {
         this.setState({
             scores: [...this.state.scores, score]
-        })
+        });
     }
-
 
     //Generate cards
     render() {
-
         //console.log(this.state.score);
         let scores = this.state.scores.map((score, i) => {
-            return(
+            return (
                 <div className="col-md-3" key={i}>
-                    <div className="card mt-4" style={{ background: '#1A1D20' }}>
-                        <div className="card-title text-center" style={{ background: '#3a3d41' }}>
-                            <div className="card-header" style={{ background: '#1A1D20' }}>
-                                        <button className="btn btn-danger btn-sm float-right" onClick={this.removeCard.bind(this, i)}>
-                                            X
-                                        </button>
-                                        <h3 style={{ color: 'gold' }}><strong>{score.player}</strong></h3>
+                    <div
+                        className="card mt-4"
+                        style={{ background: "#1A1D20" }}
+                    >
+                        <div
+                            className="card-title text-center"
+                            style={{ background: "#3a3d41" }}
+                        >
+                            <div
+                                className="card-header"
+                                style={{ background: "#1A1D20" }}
+                            >
+                                <button
+                                    className="btn btn-danger btn-sm float-right"
+                                    onClick={this.removeCard.bind(this, i)}
+                                >
+                                    X
+                                </button>
+                                <h3 style={{ color: "gold" }}>
+                                    <strong>{score.player}</strong>
+                                </h3>
                             </div>
-                            <h3 style={{ color: 'f1f1f1' }}>Ace: {score.ace}</h3>
-                            <h3 style={{ color: 'f1f1f1' }}>King: {score.king}</h3>
-                            <h3 style={{ color: 'f1f1f1' }}>Queen: {score.queen}</h3>
-                            <h3 style={{ color: 'f1f1f1' }}>Jack: {score.jack}</h3>
-                            <h3 style={{ color: 'f1f1f1' }}>Red: {score.red}</h3>
-                            <h3 style={{ color: 'f1f1f1' }}>Black: {score.black}</h3>
-                            <div className="card-header" style={{ background: '#1A1D20' }}>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                Ace: {score.ace}
+                            </h3>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                King: {score.king}
+                            </h3>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                Queen: {score.queen}
+                            </h3>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                Jack: {score.jack}
+                            </h3>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                Red: {score.red}
+                            </h3>
+                            <h3 style={{ color: "f1f1f1" }}>
+                                Black: {score.black}
+                            </h3>
+                            <div
+                                className="card-header"
+                                style={{ background: "#1A1D20" }}
+                            >
                                 <span className="badge badge-pill badge-danger ml-2">
-                                    Total: {parseInt(score.ace, 10)  + parseInt(score.king, 10) + parseInt(score.queen, 10) + parseInt(score.jack, 10) + parseInt(score.red, 10) + parseInt(score.black, 10)}
+                                    Total:{" "}
+                                    {parseInt(score.ace, 10) +
+                                        parseInt(score.king, 10) +
+                                        parseInt(score.queen, 10) +
+                                        parseInt(score.jack, 10) +
+                                        parseInt(score.red, 10) +
+                                        parseInt(score.black, 10)}
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
-            )
-        })
+            );
+        });
 
         //Score main
         return (
@@ -110,25 +134,31 @@ class Score extends Component {
                     <div className="row mt-4" style={{ width: "100%" }}>
                         <div className="col mt-4">
                             <span>
-                                <AddCard onAddCard={this.handleAddCard}/>
+                                <AddCard onAddCard={this.handleAddCard} />
                             </span>
                         </div>
                         <div className="col mt-4">
-                            <h1 style={{ color: 'gold' }}><strong>Score Mode</strong></h1>
+                            <h1 style={{ color: "gold" }}>
+                                <strong>Score Mode</strong>
+                            </h1>
                         </div>
-                        <div className="col mt-4" style={{ textAlign: "right" }} >
+                        <div
+                            className="col mt-4"
+                            style={{ textAlign: "right" }}
+                        >
                             <span className="badge badge-pill badge-warning ml-2">
-                                Players: { this.state.scores.length }
+                                Players: {this.state.scores.length}
                             </span>
                         </div>
                     </div>
                     <div className="container">
                         <div className="col mt-4">
-                            <EditCard onEditCard={this.handleEditCard} estado={this.state.scores}/>
+                            <EditCard
+                                onEditCard={this.handleEditCard}
+                                estado={this.state.scores}
+                            />
                         </div>
-                        <div className="row mt-4">
-                            {scores}
-                        </div>
+                        <div className="row mt-4">{scores}</div>
                     </div>
                 </header>
             </div>
