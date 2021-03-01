3690c2d840fc147209e55f1a04bbe9a242c057d2
AlexTheMagnus <i72lulem@uco.es>
2021-03-01 21:35:03 +0100

    Update player reducer and delete some useless files

--- src/reducers/players.js
+++ src/reducers/players.js
@@ -3,14 +3,38 @@ import { players } from '../players.json';
 const playersReducer = (state = players, action) => {
   switch (action.type) {
     case 'ADDSCORE':
+      const playerToScore = action.payload.playerName;
+      const diceFace = action.payload.diceFace;
+      const points = action.payload.points;
+
+      for (var i = 0; i < state.length; i++) {
+        if (state[i].name === playerToScore) {
+          state[i].score[diceFace] = points;
+        }
+      }
+
       return state;
 
     case 'CHANGEAVATAR':
-      const playerPosition = action.payload.playerPosition;
+      const playerToEdit = action.payload.playerToEdit;
       const numberOfAvatars = action.payload.numberOfAvatars;
 
-      state[playerPosition].avatar =
-        (state[playerPosition].avatar + 1) % numberOfAvatars;
+      for (var i = 0; i < state.length; i++) {
+        if (state[i].name === playerToEdit) {
+          state[i].avatar = (state[i].avatar + 1) % numberOfAvatars;
+        }
+      }
+
+      return state;
+
+    case 'DELETEPLAYER':
+      const playerToDelete = action.payload.playerName;
+
+      for (var i = 0; i < state.length; i++) {
+        if (state[i].name === playerToDelete) {
+          state.splice(i, 1);
+        }
+      }
 
       return state;
 
